c85a8680ef88b8bfb7b3c9f7c5b2563ea05ac4f6
lesterarte <lesterarte007@unitec.edu>
2016-08-28 01:35:49 -0600

    bueno entendiendo que hay que hacer

--- Proyecto_Grafos/src/proyecto_grafos/Proyecto_Grafos.java
+++ Proyecto_Grafos/src/proyecto_grafos/Proyecto_Grafos.java
@@ -1,10 +1,33 @@
 
 package proyecto_grafos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.*;
 public class Proyecto_Grafos {
+   
+    
+    
 
     public static void main(String[] args) {
+       
+       FileNameExtensionFilter filter = new  FileNameExtensionFilter("txt","txt");
+       JFileChooser FSArchivo = new JFileChooser();
+       FSArchivo.setFileFilter(filter);
+       int opcion = FSArchivo.showDialog(FSArchivo, "aceptar");
+       
         
+        
+       Graph newGrafo = new SingleGraph("Grafo 1");  
+       newGrafo.addNode("A");
+       newGrafo.addNode("B");
+       newGrafo.addEdge("AB", "A", "B");
+       newGrafo.addNode("C");
+           
     }
     
 }
